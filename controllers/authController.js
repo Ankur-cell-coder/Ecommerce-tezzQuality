e23a357114e5fb9054b4505248a542c22f84aea2
Ankur-cell-coder <ankurbgs6243@gmail.com>
2023-05-12 22:48:34 +0530

    login and register functionality implemented successfully

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,2 +1,113 @@
-export const registerController=()=>{};
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+import userModel from "../models/userModel.js";
+import JWT from "jsonwebtoken";
 
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+
+    if (!name) {
+      return res.send({ error: "Name is required" });
+    }
+    if (!email) {
+      return res.send({ error: "Email is required" });
+    }
+    if (!phone) {
+      return res.send({ error: "Phone no is required" });
+    }
+    if (!password) {
+      return res.send({ error: "Password is required" });
+    }
+    if (!address) {
+      return res.send({ error: "Address is required" });
+    }
+
+    //existing user
+    const existinguser = await userModel.findOne({ email });
+
+    if (existinguser) {
+      return res.status(200).send({
+        success: true,
+        error: "Email already exist please login",
+      });
+    }
+
+    //register user
+    const hashedPassword = await hashPassword(password);
+    //save
+    const user = await new userModel({
+      name,
+      email,
+      password: hashedPassword,
+      phone,
+      address,
+    }).save();
+
+    res.status(201).send({
+      success: true,
+      message: "User Successfully registered",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Registration",
+      error,
+    });
+  }
+};
+
+//POST || LOGIN
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    //check user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Please Register first before login",
+      });
+    }
+
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: "Password not matched",
+      });
+    }
+    //token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+    res.status(200).send({
+      success: true,
+      message: "Login Successfull",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+        role: user.role,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Login Failed",
+      error,
+    });
+  }
+};
